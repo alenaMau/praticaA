7cd7b1d0cb4a5faeb59cd3498e97a4b33ed7458e
alenaMau <145892396+alenaMau@users.noreply.github.com>
2024-03-25 17:02:01 +0700

    Update habr_cypress_test.spec.js

--- cypress/integration/habr_cypress_test.spec.js
+++ cypress/integration/habr_cypress_test.spec.js
@@ -5,10 +5,10 @@ describe('Cypress Test',() => {
             cy.visit(data.main_url)
 
             cy.log('Ввод несуществующего логина')
-            cy.get('input[id="pass_field_login"]')
+            cy.get('input[id="passp-field-login"]')
                 .type(data.none_existent_login)
             cy.log('Клик по кнопке "Войти')
-            cy.get('div[class="pass-button passp-in-button"] button[type="submit"]')
+            cy.get('div[class="passp-button passp-in-button"] button[type="submit"]')
                 .click()
             cy.log('Проверка что появилсяэлемент сигнализирующий об ощибке')
             cy.get('div[class="pass-form-field_error"]')
