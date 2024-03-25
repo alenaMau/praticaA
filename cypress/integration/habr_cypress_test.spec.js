bc3025132c5336ef8d7962ba9175d5aa3754be7c
Алёна Зеленина <gr423_zeaol@lab-308-1.tomtit-tomsk.ru>
2024-03-25 11:10:23 +0700

    Initial commit

---
+++ cypress/integration/habr_cypress_test.spec.js
@@ -0,0 +1,18 @@
+describe('Cypress Test',() => {
+    it ('None-existent login test',() => {
+        cy.fixture('cypressTest').then(data => {
+            cy.log('Переxод на страницу автозации')
+            cy.visit(data.main_url)
+
+            cy.log('Ввод несуществующего логина')
+            cy.get('input[id="pass_field_login"]')
+                .type(data.none_existent_login)
+            cy.log('Клик по кнопке "Войти')
+            cy.get('div[class="pass-button passp-in-button"] button[type="submit"]')
+                .click()
+            cy.log('Проверка что появилсяэлемент сигнализирующий об ощибке')
+            cy.get('div[class="pass-form-field_error"]')
+                .should('exist')
+        })
+    })
+})
